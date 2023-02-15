ba9d5c4deef6d12eb551532d6923b45db0fd7f43
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-02-16 01:58:18 +0700

    Revert "Update deployAsTask.ps1"
    This reverts commit 3abcf8c5559d26029e68c8c344c14561c5eb1627.

--- script/deployAsTask.ps1
+++ script/deployAsTask.ps1
@@ -8,7 +8,7 @@ $url = $args[0]
 
 $action = New-ScheduledTaskAction -Execute "$current_dir\daemon.exe" -Argument "--url $url" -WorkingDirectory $current_dir
 $trigger = New-ScheduledTaskTrigger -AtLogon
-$principal = New-ScheduledTaskPrincipal -GroupId "NT AUTHORITY\SYSTEM" -RunLevel Highest
+$principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
 $settings = New-ScheduledTaskSettingsSet -Hidden
 $task = New-ScheduledTask -Action $action  -Trigger $trigger -Settings $settings -Principal $principal
 
